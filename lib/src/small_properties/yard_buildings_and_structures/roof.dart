cb52a2196cf4b1dfc45d842dbe5c65c76fe9e499
Milla <milla_kp@hotmail.com>
2025-03-28 08:03:19 +0200

    Yard  buildings and structurers started

---
+++ lib/src/small_properties/yard_buildings_and_structures/roof.dart
@@ -0,0 +1,105 @@
+import 'package:bl_demolition_materials/src/data_types/roof_type_small_properties.dart';
+import 'package:bl_demolition_materials/src/data_types/water_roof_type.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:bl_demolition_materials/src/small_properties/material_weight.dart';
+import 'package:bl_demolition_materials/src/utils/utils.dart';
+
+part 'roof.freezed.dart';
+
+/// Piharakennukset ja rakenteet
+/// Katto
+
+@freezed
+abstract class Roof with _$Roof {
+  const Roof._();
+
+  const factory Roof(
+      {RoofTypeSmallProperties? roofType,
+      WaterRoofType? waterRoofType,
+      num? lapelLengthInMeters,
+      num? lapelWidthInMeters}) = _Roof;
+
+  /// Kattopinta-ala (m2)
+  num? get roofArea {
+    if (roofType == RoofTypeSmallProperties.gableRoof) {
+      return Utils.multiplyOrNull([lapelLengthInMeters, lapelWidthInMeters, 2]);
+    } else if (roofType == RoofTypeSmallProperties.flatRoof) {
+      return Utils.multiplyOrNull([lapelLengthInMeters, lapelWidthInMeters]);
+    } else if (roofType == RoofTypeSmallProperties.pentRoof) {
+      return Utils.multiplyOrNull([lapelLengthInMeters, lapelWidthInMeters]);
+    }
+    return null;
+  }
+
+  /// Puisen ristikkorakenteen paino (tonnia)
+  num? get woodenRoofLatticeWeightTons {
+    if (roofType == RoofTypeSmallProperties.gableRoof) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        WoodenRoofStructuresWeights.gableRoofWithLattice800mmKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    } else if (roofType == RoofTypeSmallProperties.flatRoof) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        WoodenRoofStructuresWeights.flatRoofWithLattice800mmKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    } else if (roofType == RoofTypeSmallProperties.pentRoof) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        WoodenRoofStructuresWeights.cofferedCeilingWithLattice800mmKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    }
+    return null;
+  }
+
+  /// Aluslaudoituksen paino (tonnia)
+  num? get underBoardingWeightTons {
+    num? multiply = Utils.multiplyOrNull([
+      roofArea,
+      WoodMaterialInfo.woodWeightPerSquareMeter(WoodMaterialType.board20x125)
+    ]);
+    return multiply != null ? multiply / 1000 : null;
+  }
+
+  /// Vesikattteen paino (tonnia)
+  num? get waterRoofWeightTons {
+    if (waterRoofType == WaterRoofType.roofingFelt) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        BuildingBoardsAndInsulationMaterialWeights.roofingFeltKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    } else if (waterRoofType == WaterRoofType.metalRoof) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        BuildingBoardsAndInsulationMaterialWeights.roofSheet06mmKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    } else if (waterRoofType == WaterRoofType.mineriteRoof) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        BuildingBoardsAndInsulationMaterialWeights.mineriteBoardKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    } else if (waterRoofType == WaterRoofType.tiledRoof) {
+      num? multiply = Utils.multiplyOrNull([
+        roofArea,
+        BuildingBoardsAndInsulationMaterialWeights.roofBrickKgPerSqm
+      ]);
+      return multiply != null ? multiply / 1000 : null;
+    }
+    return null;
+  }
+
+  /// Aluskate (tonnia)
+  num? get underlaymentWeightTons {
+    num? multiply = Utils.multiplyOrNull([
+      roofArea,
+      BuildingBoardsAndInsulationMaterialWeights.underlaymentKgPerSqm
+    ]);
+    return multiply != null ? multiply / 1000 : null;
+  }
+}
