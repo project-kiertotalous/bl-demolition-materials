9a39776f48cf60572cab3b3c11d795ff97f00e89
Milla <milla_kp@hotmail.com>
2025-03-28 15:05:42 +0200

    fix to multiplyOrZero

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -47,8 +47,7 @@ class Utils {
   /// Given a list of nullable numbers, returns 0 if every number is null.
   /// Otherwise, treats every null value as 0 and multiplies the result.
   static num? multiplyOrZero(List<num?> values) {
-    num sum = values.fold<num>(0, (acc, val) => acc * (val ?? 0));
-    return sum;
+    return values.fold<num>(1, (acc, val) => acc * (val ?? 0));
   }
 
   /// Given a mapping function, aggregates the list of elements. Returns null if
