67a15fda3f9f976452107f3df8d8da80111700a3
Milla <milla_kp@hotmail.com>
2025-04-02 12:00:16 +0300

    Huoneisto-part of Perustus and ulkovaippa done

---
+++ lib/src/small_properties/foundations_and_outer_shell/apartments.dart
@@ -0,0 +1,47 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../apartments/apartment_size.dart';
+import '../apartments/apartment.dart';
+
+part 'apartments.freezed.dart';
+
+/// Huoneistot
+@freezed
+abstract class Apartments with _$Apartments {
+  const Apartments._();
+
+  const factory Apartments(
+      {required ApartmentSize apartmentSize,
+      required Apartment apartment}) = _Apartments;
+
+  /// Huoneiston koko
+  /// 1h, kpl
+  num? get oneRoomPcs => apartmentSize.oneRoom?.pcsPerHouse;
+
+  /// 1h, lattia-ala (m2)
+  num? get oneRoomFloorArea => apartmentSize.oneRoom?.floorAreaPerApartment;
+
+  /// 2h, kpl
+  num? get twoRoomPcs => apartmentSize.twoRooms?.pcsPerHouse;
+
+  /// 2h, lattia-ala (m2)
+  num? get twoRoomFloorArea => apartmentSize.twoRooms?.floorAreaPerApartment;
+
+  /// 3h, kpl
+  num? get threeRoomPcs => apartmentSize.threeRooms?.pcsPerHouse;
+
+  /// 3h, lattia-ala (m2)
+  num? get threeRoomFloorArea =>
+      apartmentSize.threeRooms?.floorAreaPerApartment;
+
+  /// 4h, kpl
+  num? get fourRoomPcs => apartmentSize.fourRooms?.pcsPerHouse;
+
+  /// 4h, lattia-ala (m2)
+  num? get fourRoomFloorArea => apartmentSize.fourRooms?.floorAreaPerApartment;
+
+  /// Yhteensä, kpl
+  num? get totalPcs => apartmentSize.totalRoomCount;
+
+  /// Yhteensä, lattia-ala (m2)
+  num? get totalFloorArea => apartmentSize.totalFloorArea;
+}
