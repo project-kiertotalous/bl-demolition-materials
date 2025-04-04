d23274b31ce63d533d59326a6c7139142ce8e384
Milla <milla_kp@hotmail.com>
2025-04-04 12:32:30 +0300

    started working on purkukohteen jatteet

---
+++ lib/src/small_properties/demolition_site_waste/small_properties_reusable_and_recyclable_materials.dart
@@ -0,0 +1,34 @@
+import '../../small_properties/quantities_to_material_form/quantities_to_material_form.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'small_properties_reusable_and_recyclable_materials.freezed.dart';
+part 'small_properties_reusable_and_recyclable_materials.g.dart';
+
+/// HYÖDYNTÄMISKELPOISET JA KIERRÄTETTÄVÄT MATERIAALIT
+@freezed
+abstract class SmallPropertiesReusableAndRecyclableMaterials
+    with _$SmallPropertiesReusableAndRecyclableMaterials {
+  SmallPropertiesReusableAndRecyclableMaterials._();
+
+  factory SmallPropertiesReusableAndRecyclableMaterials(
+          {num? cleanSoilDemolitionCost,
+          num? asphaltWasteDemolitionCost,
+          num? cleanConcreteDemolitionCost,
+          num? reinforcedConcreteSheetMetalRoofAndMoreDemolitionCost,
+          num? wallAndCeilingTilesDemolitionCost,
+          num? cleanReusableWoodDemolitionCost,
+          num? burnableWoodWasteDemolitionCost,
+          num? windProtectionSheetBitumenOrSimilarDemolitionCost,
+          num? glassAndMineralInsulationWoolDemolitionCost,
+          num? cybrocSheetsDemolitionCost,
+          num? chipboardDemolitionCost,
+          num? energyWastePaintedWoodRoofingFeltAndUnderLaymentDemolitionCost,
+          num? glassDemolitionCost,
+          num? aluminiumDemolitionCost,
+          num? otherMetalScrapDemolitionCost,
+          num? stainlessSteelDemolitionCost,
+          num? copperDemolitionCost,
+          num? concreteYardTilesAndStonesDemolitionCost,
+          num? overallReusableMaterialsDemolitionCost}) =
+      _ReusableAndRecyclableMaterials;
+}
